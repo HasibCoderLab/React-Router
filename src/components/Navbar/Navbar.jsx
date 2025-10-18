42ded19f6a1a5278642122454b27d203afb671ca
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-18 22:02:29 +0600

    add styly in nabvar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,8 +4,12 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div>
-       <ul>
+       <ul className='bg-green-800 text-3xl m-2 p-4 flex justify-between'>
         <Link to={'/home'}>Home</Link>
+        <Link to={'/home'}>About</Link>
+        <Link to={'/home'}>Blogs</Link>
+        <Link to={'/home'}>Products</Link>
+        <Link to={'/home'}>Contact</Link>
        </ul>
     </div>
   )
